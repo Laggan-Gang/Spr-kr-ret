f1a7eeecb8b8a00f55929cfb353d782b86594075
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-01-22 23:20:49 +0100

    Mer grejer

--- språk.js
+++ språk.js
@@ -42,7 +42,6 @@ function gif(meddelande, dravel, channel) {
     }
   }
   webbKrok();
-  meddelande.delete();
 }
 
 // When the client is ready, run this code (only once)
@@ -50,15 +49,47 @@ client.once("ready", () => {
   console.log("Språkröret is online");
 });
 
+var gifSamling = {
+  aaa: "A spooky skeleton",
+  asg: "Big bro rolls around",
+  cdd: "Cool dog checks it out",
+  cia: "BaaaaaaanEE?",
+  cil: "Bane ;P",
+  cmm: "Prince Charming is not having it",
+  cpr: "Cute panda rolls",
+  fih: "Arkosh fanboy gathering",
+  fuu: "Big bro is not pleased",
+  jdk: "We just don't know",
+  mam: "Hot mama!",
+  ooh: "*Jaw Drops* Awooga! / Hummina Hummina Bazooing!",
+  sry: "Oh shit",
+};
+
 client.on("messageCreate", async (meddelande) => {
   //=> är en funktion
   const channel = client.channels.cache.get(meddelande.channel.id);
   let dravel = meddelande.content.toLocaleLowerCase();
 
   if (!meddelande.author.bot) {
-    if (dravel == ":cmm:") {
+    if (dravel.slice(1, 4) in gifSamling) {
+      console.log("Vi hittade grejen därine");
       gif(meddelande, dravel, channel);
+    } else if (dravel === "oh my, what are the codes for these gifs?") {
+      let gifManuel = new String();
+      for (gifBeskrivning in gifSamling) {
+        console.log(gifBeskrivning + gifSamling[gifBeskrivning]);
+        gifManuel +=
+          "\n" +
+          ":" +
+          gifBeskrivning +
+          ":" +
+          " = " +
+          gifSamling[gifBeskrivning];
+      }
+
+      meddelande.reply(gifManuel);
     }
+    console.log("vi hittade inte den här grejen");
   }
 });
 
